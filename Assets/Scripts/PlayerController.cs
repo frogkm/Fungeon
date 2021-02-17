9591b248d7449705eb1b1ce28ea282ac46231ccf
frogkm <53508449+frogkm@users.noreply.github.com>
2021-02-17 02:46:25 -0500

    Started the project!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public Transform transform;
+  public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      transform = GetComponent<Transform>();
+      speed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.W)){
+         transform.position = transform.position + new Vector3(0, speed, 0);
+       }
+       if (Input.GetKey(KeyCode.A)){
+         transform.position = transform.position + new Vector3(-speed, 0, 0);
+       }
+       if (Input.GetKey(KeyCode.S)){
+         transform.position = transform.position + new Vector3(0, -speed, 0);
+       }
+       if (Input.GetKey(KeyCode.D)){
+         transform.position = transform.position + new Vector3(speed, 0, 0);
+       }
+    }
+}

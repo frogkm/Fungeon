31493e21d43f481e8118bf2dda88ffd87e43913a
frogkm <53508449+frogkm@users.noreply.github.com>
2021-02-18 03:16:45 -0500

    BUGGED

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,9 +11,11 @@ public class PlayerController : MonoBehaviour
   public float speed;
   public Vector3 mousePosition;
 
-
   float xVel = 0;
   float yVel = 0;
+
+  public GameObject bullet;
+  public float bulletSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -42,15 +45,64 @@ public class PlayerController : MonoBehaviour
        }
 
        rb.velocity = new Vector2(xVel, yVel);
+
+       // Rotation
        mousePosition = Input.mousePosition;
        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-       //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
-       //Transform pos = transform.TransformDirection(Vector3.forward * 3);
-       //transform.LookAt(new Transform(Input.mousePosition));
-       Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward );
+       Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
        transform.rotation = rot;
        transform.eulerAngles = new Vector3(0, 0,transform.eulerAngles.z);
+
+
+       if (Input.GetKey(KeyCode.Mouse0)){
+
+         GameObject e = Instantiate(bullet) as GameObject;
+         e.transform.position = transform.position;
+         Rigidbody2D bulletRb = e.GetComponent<Rigidbody2D>();
+         float angle = transform.eulerAngles.z;
+         float xSign = 1;
+         float ySign = 1;
+
+         if (angle > -90){
+           angle += 90;
+         }
+         if (angle < -90){
+           angle += 450;
+         }
+
+         if (angle > 90 && angle < 180){
+           xSign = -1;
+         }
+         else if (angle > 180 && angle < 270){
+           xSign = -1;
+           ySign = -1;
+         }
+         else if (angle > 270){
+           ySign = -1;
+         }
+
+
+         //if(angle < 0){
+        //    angle = 360 + angle;
+         //}
+      //   if (angle > 90){
+    //       angle = 180 - angle;
+      //   }
+      //   else if (angle < -90){
+      //     angle = 90 + angle;
+      //   }
+
+         float bulletYVel = ySign * bulletSpeed * (float)Math.Sin(angle);
+         float bulletXVel = xSign * bulletSpeed * (float)Math.Cos(angle);
+
+         bulletRb.velocity = new Vector2(bulletXVel, bulletYVel);
+
+       }
+       //Debug.Log();
+
+
+
+
 
     }
 }

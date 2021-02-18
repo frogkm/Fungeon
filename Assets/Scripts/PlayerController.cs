4911728890b20501c2b81624b632633a88d32778
frogkm <53508449+frogkm@users.noreply.github.com>
2021-02-18 01:30:16 -0500

    Added working collision and rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,32 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+  public Rigidbody2D rb;
   public Transform transform;
   public float speed;
+  public Vector3 mousePosition;
+
+
+  float xVel = 0;
+  float yVel = 0;
 
     // Start is called before the first frame update
     void Start()
     {
       transform = GetComponent<Transform>();
-      speed = 0.05f;
+      rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+      xVel = 0;
+      yVel = 0;
+
        if (Input.GetKey(KeyCode.W)){
-         transform.position = transform.position + new Vector3(0, speed, 0);
+         yVel += speed;
        }
        if (Input.GetKey(KeyCode.A)){
-         transform.position = transform.position + new Vector3(-speed, 0, 0);
+         xVel -= speed;
        }
        if (Input.GetKey(KeyCode.S)){
-         transform.position = transform.position + new Vector3(0, -speed, 0);
+         yVel -= speed;
        }
        if (Input.GetKey(KeyCode.D)){
-         transform.position = transform.position + new Vector3(speed, 0, 0);
+         xVel += speed;
        }
+
+       rb.velocity = new Vector2(xVel, yVel);
+       mousePosition = Input.mousePosition;
+       mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+       //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
+       //Transform pos = transform.TransformDirection(Vector3.forward * 3);
+       //transform.LookAt(new Transform(Input.mousePosition));
+       Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward );
+       transform.rotation = rot;
+       transform.eulerAngles = new Vector3(0, 0,transform.eulerAngles.z);
+
     }
 }

a978aa2c90c46d61bd315397bdda40377517f27a
frogkm <53508449+frogkm@users.noreply.github.com>
2021-02-18 12:16:09 -0500

    Fixed bullet angle, cleaned up code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,100 +9,80 @@ public class PlayerController : MonoBehaviour
   public Rigidbody2D rb;
   public Transform transform;
   public float speed;
-  public Vector3 mousePosition;
-
-  float xVel = 0;
-  float yVel = 0;
-
   public GameObject bullet;
   public float bulletSpeed;
+  public int shotsPerSec;
+
+  Vector3 mousePosition;
+  float xVel;
+  float yVel;
+  float bulletYVel;
+  float bulletXVel;
+  float angle;
+
+    //Rotates the transform's z plane to point it towards position
+    void lookAt(Transform transform, Vector3 position){
+      position = Camera.main.ScreenToWorldPoint(position);
+      Quaternion rot = Quaternion.LookRotation(transform.position - position, Vector3.forward);
+      transform.rotation = rot;
+      transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+
+    void setMovement(Rigidbody2D rb){
+      xVel = 0;
+      yVel = 0;
+
+      if (Input.GetKey(KeyCode.W)){
+        yVel += speed;
+      }
+      if (Input.GetKey(KeyCode.A)){
+        xVel -= speed;
+      }
+      if (Input.GetKey(KeyCode.S)){
+        yVel -= speed;
+      }
+      if (Input.GetKey(KeyCode.D)){
+        xVel += speed;
+      }
+
+      rb.velocity = new Vector2(xVel, yVel);
+    }
+
+    void shootBullet(){
+      GameObject e = Instantiate(bullet) as GameObject;
+      e.transform.position = transform.position;
+      Rigidbody2D bulletRb = e.GetComponent<Rigidbody2D>();
+      angle = transform.eulerAngles.z;
+
+      angle = angle + 90;
+      if (angle > 360){
+        angle = angle - 360;
+      }
+
+      angle = (angle * (float)(Math.PI)) / 180;
+
+      bulletYVel = bulletSpeed * (float)Math.Sin(angle);
+      bulletXVel = bulletSpeed * (float)Math.Cos(angle);
+
+      bulletRb.velocity = new Vector2(bulletXVel, bulletYVel);
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
       transform = GetComponent<Transform>();
       rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-      xVel = 0;
-      yVel = 0;
-
-       if (Input.GetKey(KeyCode.W)){
-         yVel += speed;
-       }
-       if (Input.GetKey(KeyCode.A)){
-         xVel -= speed;
-       }
-       if (Input.GetKey(KeyCode.S)){
-         yVel -= speed;
-       }
-       if (Input.GetKey(KeyCode.D)){
-         xVel += speed;
-       }
-
-       rb.velocity = new Vector2(xVel, yVel);
-
-       // Rotation
-       mousePosition = Input.mousePosition;
-       mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-       Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
-       transform.rotation = rot;
-       transform.eulerAngles = new Vector3(0, 0,transform.eulerAngles.z);
-
-
+       setMovement(rb);
+       lookAt(transform, Input.mousePosition);
        if (Input.GetKey(KeyCode.Mouse0)){
-
-         GameObject e = Instantiate(bullet) as GameObject;
-         e.transform.position = transform.position;
-         Rigidbody2D bulletRb = e.GetComponent<Rigidbody2D>();
-         float angle = transform.eulerAngles.z;
-         float xSign = 1;
-         float ySign = 1;
-
-         if (angle > -90){
-           angle += 90;
-         }
-         if (angle < -90){
-           angle += 450;
-         }
-
-         if (angle > 90 && angle < 180){
-           xSign = -1;
-         }
-         else if (angle > 180 && angle < 270){
-           xSign = -1;
-           ySign = -1;
-         }
-         else if (angle > 270){
-           ySign = -1;
-         }
-
-
-         //if(angle < 0){
-        //    angle = 360 + angle;
-         //}
-      //   if (angle > 90){
-    //       angle = 180 - angle;
-      //   }
-      //   else if (angle < -90){
-      //     angle = 90 + angle;
-      //   }
-
-         float bulletYVel = ySign * bulletSpeed * (float)Math.Sin(angle);
-         float bulletXVel = xSign * bulletSpeed * (float)Math.Cos(angle);
-
-         bulletRb.velocity = new Vector2(bulletXVel, bulletYVel);
-
+         shootBullet();
        }
-       //Debug.Log();
-
-
-
-
 
     }
 }
